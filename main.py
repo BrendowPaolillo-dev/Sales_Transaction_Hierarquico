4c934c6525d0cff2c5657a2000141de930922fd8
RoyaleCheese <brendowpaolillo@gmail.com>
2018-11-25 16:54:06 -0200

    corrigida a leitura dos dados

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import csv
-if __name__ =='__main__':
+
+def get_data():
+    newfile = open ("noheadertable.txt", "r+")
+    aux_list = []
     with open('Sales_Transactions_Dataset_Weekly.csv', 'rb') as csvfile:
-        spamreader = csv.reader(csvfile)
+        spamreader = csv.reader(csvfile, delimiter=',')
+        next(spamreader, None)
         for row in spamreader:
-            print ', '.join(row)
+            vector =[]
+            for j in range (1, 53):
+                vector.append(row[j])
+            aux_list.append(vector)
+    for i in range (len(aux_list)-1):    
+        newfile.write(str(aux_list[i]))
+    newfile.close()
+    return aux_list
+
+def main():
+    data = get_data()
+    print data[0]
+if __name__ =='__main__':
+    main()
