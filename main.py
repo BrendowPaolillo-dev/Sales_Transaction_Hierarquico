60a7b1d194ff4cafb6da6a500460cd05da2c41a2
RoyaleCheese <brendowpaolillo@gmail.com>
2018-11-30 12:22:16 -0200

    resolucao final

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import scipy.cluster.hierarchy as sch
 from scipy.spatial.distance import pdist
 from scipy.spatial import distance
 from matplotlib import pyplot as plt
-from sklearn.cluster import AgglomerativeClustering
-
-
 
 def get_data():
     newfile = open ("noheadertable.txt", "r+")
@@ -28,20 +25,6 @@ def get_data():
 def main():
     data = get_data()
     data = np.array(data)
-    """w = []
-    j = 0
-    for j in range(len(data)):
-        i = 0
-        w.append([])
-        s = 0
-        while i < len(data[j]):
-            s += data[j][i]
-            s += data[j][i+1]
-            s += data[j][i+2]
-            s += data[j][i+3]
-            w[j].append(s)
-            s = 0
-            i += 4"""
 
     for j in range(len(data)):
         s = 0
@@ -51,22 +34,10 @@ def main():
         i = 0
         for i in range(len(data[j])):
             data[j][i] = data[j][i]/s
-    #print w
+
     X = pdist(data, 'euclidean')
-    #print distance.euclidean(data[2], data[3])
-    #print distance.euclidean(data[2], data[7])
-    #print distance.euclidean(data[2], data[17])
-    #print distance.euclidean(data[17], data[18])
-
-
-    #print X
     Z = sch.linkage(X, 'average')
-    #print Z
-    #c = AgglomerativeClustering(n_clusters=10, affinity='euclidean')
-    #print (c.fit_predict(data))
-    A = sch.fcluster(Z, t=0.4, criterion='distance')
-    print np.sort(A)
-    print len(A)
+    
     fig = plt.figure(figsize=(25, 10))
     dn = sch.dendrogram(Z)
     plt.show()
